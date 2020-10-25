8e1438433d308cc4ef56252456f1fcac7f7715f9
Johnatan Chaves Ribeiro <johnatan.jcr@gmail.com>
2020-10-25 00:01:02 -0300

    projeto final 1 aula 2

--- ProjetoFinal1/saraiva.py
+++ ProjetoFinal1/saraiva.py
@@ -14,11 +14,30 @@ def get_http(url, nome_livro):
     except Exception as e:
         raise 
 
+def get_produto(content):
+    soup = BeautifulSoup(content, 'lxml')
+    produtos = soup.find_all('li', {'class':'nm-product-item'})
+
+    lista_produtos = []
+    for produto in produtos:
+        info_produto = [produto.a.get('href'), produto.a.get('title')]
+        lista_produtos.append(info_produto)
+    return(lista_produtos)
+
+
 if __name__ == '__main__':
     url = 'https://busca.saraiva.com.br/'
     nome = 'Harry Potter'
 
     r = get_http(url, nome)
+    if r: 
+        lista_produtos = get_produto(r.text)
+        print(lista_produtos)
 
+
+
+'''
+    não vamos mais escrever em arquivo
     with open('result.html', 'w', encoding='utf8') as f:
         f.write(r.text)
+'''
